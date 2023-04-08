9a5da0055e9cf4a5689e65dc3cab308bb42bd92c
Siddharth Kundu <sk31395@gmail.com>
2023-04-08 12:44:53 +0530

    initial project structure

--- main.go
+++ main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sid995/ecommerce/controllers"
+	"github.com/sid995/ecommerce/database"
+	"github.com/sid995/ecommerce/middleware"
+	"github.com/sid995/ecommerce/routes"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	app := controllers.NewApplication(
+		database.ProductData(database.Client, "Products"),
+		database.UserData(database.Client, "Users"),
+	)
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	router.GET("/addtocart", app.AddToCart())
+	router.GET("/removeitemfromcart", app.RemoveItem())
+	router.GET("/cartcheckout", app.BuyFromCart())
+	router.GET("/instantbuy", app.InstantBuy())
+
+	log.Fatal(router.Run(":" + port))
+}
