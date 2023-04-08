20f2af4e61c0ae8b98e8a4cc9ff7cc87441505ac
Siddharth Kundu <sk31395@gmail.com>
2023-04-08 12:25:56 +0530

    initial commit

---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sid995/ecommerce/controllers"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/login", controllers.SignUp())
+	incomingRoutes.POST("/users/signup", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SeaerchProductByQuery())
+}
